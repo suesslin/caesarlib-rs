afb26d674e979f68fb47950cf6a55612bd25d81a
Lukas Andreas Muller <lamuller@protonmail.com>
2016-12-18 06:08:58 -0800

    Commit initially

---
+++ src/main.rs
@@ -0,0 +1,79 @@
+fn main() {
+    println!("{}", caesarify("HELLO", 20));
+}
+
+fn key_v(starting: &str, key: i32) -> Vec<char> {
+    let k: i32 = key;
+    let s = starting;
+
+    let s_v: Vec<char> = s.chars().collect();
+    let mut new_v: Vec<char> = Vec::new();
+
+    let mut i: i32 = k-1;
+
+    while i < (s_v.len() as i32) {
+        new_v.push(s_v[(i as usize)]);
+        i += 1;
+    }
+
+    let mut new_i = 0;
+    while new_i <= k-2 {
+        new_v.push(s_v[(new_i as usize)]);
+        new_i += 1;
+    }
+
+    return new_v;
+}
+
+fn l_to_n(l: char) -> i16 {
+    match l {
+        'A' => 0,
+        'B' => 1,
+        'C' => 2,
+        'D' => 3,
+        'E' => 4,
+        'F' => 5,
+        'G' => 6,
+        'H' => 7,
+        'I' => 8,
+        'J' => 9,
+        'K' => 10,
+        'L' => 11,
+        'M' => 12,
+        'N' => 13,
+        'O' => 14,
+        'P' => 15,
+        'Q' => 16,
+        'R' => 17,
+        'S' => 18,
+        'T' => 19,
+        'U' => 20,
+        'V' => 21,
+        'W' => 22,
+        'X' => 23,
+        'Y' => 24,
+        'Z' => 25,
+        _ => 30
+    }
+}
+
+fn caesarify(word: &str, key: i32) -> String {
+    let starting = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    let key_v = key_v(starting, key);
+
+    let mut index_v: Vec<i16> = Vec::new();
+
+    for l in word.to_uppercase().chars() {
+        index_v.push(l_to_n(l));
+    }
+
+    let mut new_w_v: Vec<char> = Vec::new();
+
+    for i in index_v {
+        new_w_v.push(key_v[(i as usize)]);
+    }
+
+    let s: String = new_w_v.into_iter().collect();
+
+    return s;
+}
