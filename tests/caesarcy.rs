8f96d5003616a0f7eec6840cf81068b4074bed92
Rémy HUBSCHER <rhubscher@mozilla.com>
2017-03-19 09:17:23 +0100

    Fix tests runner.

--- tests/caesarcy.rs
+++ tests/caesarcy.rs
@@ -1,17 +1,21 @@
 extern crate caesarcy;
 
+use caesarcy::{encipher, decipher};
+
+
 #[cfg(test)]
 mod tests {
+    use super::*;
     #[test]
     fn ciphered_text_can_be_deciphered() {
-        assert_eq!("Toto", caesarcy::decipher(13, caesarcy::encipher(13, "Toto")));
+        assert_eq!("Toto", decipher(13, &encipher(13, "Toto")));
     }
     #[test]
     fn ciphered_text_is_different_with_regards_to_offset() {
-        assert_ne!(caesarcy::encipher(13, "Toto"), caesarcy::encipher(14, "Toto"));
+        assert_ne!(encipher(13, "Toto"), encipher(14, "Toto"));
     }
     #[test]
     fn ciphered_text_can_contains_unknown_chars() {
-        assert_eq!("Foo:bar.", caesarcy::decipher(13, caesarcy::encipher(13, "Foo:bar.")));
+        assert_eq!("Foo:bar.", decipher(13, &encipher(13, "Foo:bar.")));
     }
 }
