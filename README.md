ffd08a4a2765d9e788d8c487494028939c25378f
Lukas A. Mueller <lukasamueller@icloud.com>
2017-03-22 21:48:54 +0100

    Prepare for 0.1.4 release

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ Add this to your `Cargo.toml`:
 
 ```toml
 [dependencies]
-caesarlib = "0.1.3"
+caesarlib = "0.1.4"
 ```
 
 and this to your crate root:
--- tests/caesarlib.rs
+++ tests/caesarlib.rs
@@ -54,7 +54,12 @@ mod tests {
 
     #[test]
     fn custom_base_sequence_correct_encipherd_and_deciphered() {
-        let base = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz".chars().rev().collect::<String>();
+        let base =
+            "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+            .chars()
+            .rev()
+            .collect::<String>();
+
         let test_word = "FooBar";
         let offset = 5;
         assert_eq!(custom_decipher(&base, offset, &custom_encipher(&base, offset, test_word)), test_word);
